dca5e26e32a80cdfe525633c29c8d87b0567b430
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2024-05-27 09:35:06 +0330

    [Macos] updated config

--- nvim/lua/binaryboy/core/autocmds.lua
+++ nvim/lua/binaryboy/core/autocmds.lua
@@ -1,25 +1,33 @@
-local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
-
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- Settings for filetypes:
 --------------------------
 
 -- Disable line length marker
-augroup('setLineLength', { clear = true })
-autocmd('Filetype', {
-  group = 'setLineLength',
-  pattern = { 'text', 'markdown', 'html', 'xhtml', 'javascript', 'typescript' },
-  command = 'setlocal cc=0'
+augroup("setLineLength", { clear = true })
+autocmd("Filetype", {
+  group = "setLineLength",
+  pattern = { "text", "markdown", "html", "xhtml", "javascript", "typescript" },
+  command = "setlocal cc=0",
 })
 
 -- Set indentation to 2 spaces
-augroup('setIndent', { clear = true })
-autocmd('Filetype', {
-  group = 'setIndent',
-  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
-    'yaml', 'lua'
+augroup("setIndent", { clear = true })
+autocmd("Filetype", {
+  group = "setIndent",
+  pattern = {
+    "xml",
+    "html",
+    "xhtml",
+    "css",
+    "scss",
+    "javascript",
+    "typescript",
+    "yaml",
+    "lua",
+    "javascriptreact",
+    "typescriptreact",
   },
-  command = 'setlocal shiftwidth=2 tabstop=2'
+  command = "setlocal shiftwidth=2 tabstop=2",
 })
-
