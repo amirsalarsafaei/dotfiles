b2251161f5e2a2b3e931a326aecf7fc748b7acb0
Amirsalar Safaei <amirs.s.g.o@gmail.com>
2024-10-04 16:31:25 +0330

    hyprland

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,6 +25,8 @@ in
   # enviroent.
   home.packages = [
     # Dev Tools
+    pkgs.pavucontrol
+    pkgs.sing-box
     pkgs.handbrake
     pkgs.wofi
     pkgs.rofi-wayland
@@ -516,8 +518,81 @@ in
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    settings = { mainBar = { battery = { format = "Bat: {capacity}%"; format-charging = "Bat: {capacity}% (charging)"; format-plugged = "Bat: {capacity}% (plugged)"; interval = 10; }; clock = { format = "{:%H:%M:%S %Y-%m-%d}"; interval = 1; }; cpu = { format = "CPU: {usage}%"; interval = 1; }; "custom/absclock" = { exec = "date +%s"; format = "{}"; interval = 1; return-type = "{}"; }; "custom/loadavg" = { exec = "cat /proc/loadavg | head -c 14"; format = "Load average: {}"; interval = 1; return-type = "{}"; }; "custom/uptime" = { exec = "uptime -p | sed 's/up //g' -"; format = "Uptime: {}"; interval = 60; return-type = "{}"; }; height = 33; idle_inhibitor = { format = "{icon}"; format-icons = { activated = "Don't idle"; deactivated = "Idling"; }; }; memory = { format = "RAM: {}%"; interval = 1; }; modules-center = [ ]; modules-left = [ "sway/workspaces" "tray" "sway/mode" ]; modules-right = [ "idle_inhibitor" "cpu" "memory" "wireplumber" "battery" "sway/language" "clock" ]; network = { format = "Net via {ifname}"; format-disconnected = "No net"; format-linked = "Net (No IP) via {ifname}"; on-click = ""; tooltip-format = "{ipaddr}/{cidr}"; }; position = "top"; "sway/mode" = { format = "<span style=\"italic\">{}</span>"; }; temperature = { format = "Temperature: {temperatureC}°C"; interval = 1; }; tray = { spacing = 10; }; wireplumber = { format = "Vol: {volume}%"; format-muted = "Vol: muted"; on-click = "pavucontrol"; scroll-step = 1; }; }; };
+    settings = {
+      mainBar =
+        {
+          layer = "top";
+          exclusive = true;
+          battery = {
+            format = " \t{capacity}%";
+            format-charging = "󰢝 \t{capacity}%";
+            format-plugged = " \t{capacity}%";
+            interval = 30;
+          };
+          clock = {
+            format = "{:%H:%M:%S}";
+            interval = 1;
+          };
+          cpu = {
+            format = " \t{usage}%";
+            interval = 5;
+          };
+          "custom/absclock" = {
+            exec = "date +%s";
+            format = "{}";
+            interval = 1;
+            return-type = "{}";
+          };
+          "custom/loadavg" = {
+            exec = "cat /proc/loadavg | head -c 14";
+            format = "Load average: {}";
+            interval = 1;
+            return-type = "{}";
+          };
+          "custom/uptime" = {
+            exec = "uptime -p | sed 's/up //g' -";
+            format = "Uptime: {}";
+            interval = 60;
+            return-type = "{}";
+          };
+          height = 33;
+          idle_inhibitor = {
+            format = "{icon}";
+            format-icons = {
+              activated = "Don't idle";
+              deactivated = "Idling";
+            };
+          };
+          memory = {
+            format = " \t{}%";
+            interval = 5;
+          };
+          modules-center = [ ];
+          modules-left = [ "hyprland/workspaces" "tray" "network" ];
+          modules-right = [ "temperature" "cpu" "memory" "wireplumber" "battery" "hyprland/language" "clock" ];
+          network = {
+            format = "Net via {ifname}";
+            format-disconnected = "No net";
+            format-linked = "Net (No IP) via {ifname}";
+            format-wifi = "{essid} ({signalStrength}%)  ";
+            tooltip-format = "{ipaddr}/{cidr}";
+          };
+          position = "top";
+          temperature = {
+            format = "\t{temperatureC}°C";
+            interval = 1;
+          };
+          tray = {
+            spacing = 10;
+          };
+          wireplumber = {
+            format = " \t{volume}%";
+            format-muted = "\tmute";
+            on-click = "pavucontrol";
+            scroll-step = 1;
+          };
+        };
 
-
+    };
   };
 }
