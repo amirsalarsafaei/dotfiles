8df414bca3b3b0297d921ea4a5e625a48f958695
Amirsalar Safaei <amirs.s.g.o@gmail.com>
2025-06-10 00:12:52 +0330

    fix: use readFile instead of loadFile for WezTerm config
    - Changed from builtins.loadFile to builtins.readFile for better compatibility
    - Removed unnecessary blank line for cleaner code formatting

--- nixos/home/modules/programs/wezterm.nix
+++ nixos/home/modules/programs/wezterm.nix
@@ -1,9 +1,8 @@
 { currentHostname, pkgs, lib, ... }:
-
 {
   programs.wezterm = {
     enable = true;
     package = pkgs.wezterm;
-    extraConfig = builtins.loadFile ./wezterm.lua;
+    extraConfig = builtins.readFile ./wezterm.lua;
   };
 }
