a58bd07a148e23a9eadee0fc1b14ed5bf45c5b93
Amirsalar Safaei <amirs.s.g.o@gmail.com>
2025-10-13 13:21:31 +0330

    fix neovim

--- nvim/lua/binaryboy/plugins/nvim-tree.lua
+++ nvim/lua/binaryboy/plugins/nvim-tree.lua
@@ -8,103 +8,52 @@ return {
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 
-		-- ensure diagnostic signs are available for nvim-tree
-		vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-
 		nvimtree.setup({
 			view = {
 				width = 35,
-				side = "left",
-				preserve_window_proportions = true,
 				relativenumber = true,
-				signcolumn = "yes",
-				float = {
-					enable = false,
-					quit_on_focus_loss = true,
-					open_win_config = {
-						relative = "editor",
-						border = "rounded",
-						width = 50,
-						height = 30,
-						row = 1,
-						col = 1,
-					},
-				},
 			},
-			prefer_startup_root = true,
-			sync_root_with_cwd = true,
-			respect_buf_cwd = true,
-			reload_on_bufenter = true,
-			hijack_netrw = true,
-			hijack_unnamed_buffer_when_opening = false,
-			hijack_cursor = false,
-			root_dirs = {},
-			select_prompts = false,
-			sort = {
-				sorter = "case_sensitive",
-				folders_first = true,
-				files_first = false,
+			-- filters
+			filters = {
+				dotfiles = false,
+				custom = { "^.git$", "^node_modules$", "^__pycache__$" },
 			},
-			update_focused_file = {
+			-- git integration
+			git = {
 				enable = true,
-				update_root = {
-					enable = false,
-					ignore_list = {},
-				},
-				exclude = false,
+				show_on_dirs = true,
 			},
-			system_open = {
-				cmd = "",
-				args = {},
-			},
-			filesystem_watchers = {
+			-- enable diagnostics (this was disabled!)
+			diagnostics = {
 				enable = true,
-				debounce_delay = 50,
-				ignore_dirs = {
-					"node_modules",
-					".git",
-					".cache",
-					"target",
-					"build",
-					"dist",
+				show_on_dirs = true,
+				show_on_open_dirs = true,
+				severity = {
+					min = vim.diagnostic.severity.HINT,
+					max = vim.diagnostic.severity.ERROR,
 				},
 			},
-			-- change folder arrow icons
+			-- modified files
+			modified = {
+				enable = true,
+				show_on_dirs = true,
+			},
+			-- renderer
 			renderer = {
-				add_trailing = false,
 				group_empty = true,
-				full_name = false,
-				highlight_git = "name",
-				highlight_diagnostics = "name",
-				highlight_opened_files = "icon",
+				highlight_git = true,
+				highlight_opened_files = "name",
 				highlight_modified = "name",
-				highlight_bookmarks = "name",
-				root_folder_label = ":~:s?$?/..?",
-				indent_width = 2,
 				indent_markers = {
 					enable = true,
-					inline_arrows = true,
-					icons = {
-						corner = "└",
-						edge = "│",
-						item = "│",
-						bottom = "─",
-						none = " ",
-					},
 				},
-				special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-				symlink_destination = true,
 				icons = {
 					show = {
-						file = true,
-						folder = true,
-						folder_arrow = true,
 						git = true,
+						folder = true,
+						file = true,
+						folder_arrow = true,
 					},
-
 					glyphs = {
 						default = "󰈚",
 						symlink = "",
@@ -132,122 +81,36 @@ return {
 					},
 				},
 			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
+			-- actions
 			actions = {
-				use_system_clipboard = true,
-				change_dir = {
-					enable = true,
-					global = false,
-					restrict_above_cwd = false,
-				},
-				expand_all = {
-					max_folder_discovery = 300,
-					exclude = {},
-				},
-				file_popup = {
-					open_win_config = {
-						col = 1,
-						row = 1,
-						relative = "cursor",
-						border = "shadow",
-						style = "minimal",
-					},
-				},
 				open_file = {
 					quit_on_open = false,
-					eject = true,
-					resize_window = true,
 					window_picker = {
-						enable = false,
-						picker = "default",
-						chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-						exclude = {
-							filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-							buftype = { "nofile", "terminal", "help" },
-						},
+						enable = true,
 					},
 				},
-				remove_file = {
-					close_window = true,
-				},
 			},
-			filters = {
+			-- update focused file
+			update_focused_file = {
 				enable = true,
-				git_ignored = false,
-				dotfiles = false,
-				git_clean = false,
-				no_buffer = false,
-				no_bookmark = false,
-				custom = {
-					".DS_Store",
-					"\\.git$",
-					"\\.pyc$",
-					"__pycache__",
-					".idea",
-					"node_modules",
-					"\\.cache$",
-					"target",
-					"build",
-					"dist",
-				},
-				exclude = {},
+				update_root = false,
 			},
-			git = {
+			-- filesystem watchers
+			filesystem_watchers = {
 				enable = true,
-				show_on_dirs = true,
-				show_on_open_dirs = true,
-				disable_for_dirs = {},
-				timeout = 400,
-				cygwin_support = false,
-			},
-			diagnostics = {
-				enable = false,
-			},
-			modified = {
-				enable = true,
-				show_on_dirs = true,
-				show_on_open_dirs = true,
-			},
-			live_filter = {
-				prefix = "[FILTER]: ",
-				always_show_folders = true,
-			},
-			tab = {
-				sync = {
-					open = false,
-					close = false,
-					ignore = {},
-				},
-			},
-			notify = {
-				threshold = vim.log.levels.INFO,
-				absolute_path = true,
-			},
-			help = {
-				sort_by = "key",
-			},
-			ui = {
-				confirm = {
-					remove = true,
-					trash = true,
-					default_yes = false,
-				},
 			},
 		})
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		-- keymaps
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 		keymap.set(
 			"n",
 			"<leader>ef",
 			"<cmd>NvimTreeFindFileToggle<CR>",
 			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 	end,
 }
