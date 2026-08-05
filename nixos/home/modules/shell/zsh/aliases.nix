25454722c9cd669b420cb1be683f9301ae5ba3ac
Fateme Tamehri <fateme.tamehri@divar.ir>
2026-08-05 18:26:06 +0330

    Fix

--- nixos/home/modules/shell/zsh/aliases.nix
+++ nixos/home/modules/shell/zsh/aliases.nix
@@ -1,6 +1,6 @@
 {
   vim = "nvim";
-  claude = "claudy";
+  claudy = "claude";
 
   gitrecent = "git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'";
   gitshort = "git rev-parse --short=8 HEAD";
